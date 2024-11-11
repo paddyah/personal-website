79dc4cac32333aca8604f093f2b98806e0472a0b
paddyah <paddy.ahimovic@gmail.com>
2024-11-11 11:36:56 -0500

    more code cleanup

--- paddy_ah.go
+++ paddy_ah.go
@@ -4,44 +4,12 @@ import (
 	"html/template"
 	"log"
 	"net/http"
-	"os"
 	"regexp"
 )
 
-type Page struct {
-	Title string
-	Body  []byte
-}
-
-var templates = template.Must(template.ParseFiles("tmpl/edit.html", "tmpl/view.html", "tmpl/index.html", "tmpl/admin.html"))
-var validPath = regexp.MustCompile("^$")
+var templates = template.Must(template.ParseFiles("tmpl/index.html", "tmpl/admin.html"))
 var validHomePath = regexp.MustCompile("^/$")
 var validAdminPath = regexp.MustCompile("^/admin/$")
-
-func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	return os.WriteFile("data/"+filename, p.Body, 0600)
-}
-
-func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
-	body, err := os.ReadFile("data/" + filename)
-	if err != nil {
-		return nil, err
-	}
-	return &Page{Title: title, Body: body}, nil
-}
-
-func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		m := validPath.FindStringSubmatch(r.URL.Path)
-		if m == nil {
-			http.NotFound(w, r)
-			return
-		}
-		fn(w, r, m[2])
-	}
-}
 
 func homePageHandler(w http.ResponseWriter, r *http.Request) {
 	m := validHomePath.FindStringSubmatch(r.URL.Path)
@@ -49,7 +17,7 @@ func homePageHandler(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	renderTemplate(w, "index", nil)
+	renderTemplate(w, "index")
 }
 
 func adminPageHandler(w http.ResponseWriter, r *http.Request) {
@@ -58,39 +26,11 @@ func adminPageHandler(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	renderTemplate(w, "admin", nil)
+	renderTemplate(w, "admin")
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
-	p, err := loadPage(title)
-	if err != nil {
-		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
-		return
-	}
-	renderTemplate(w, "view", p)
-}
-
-func editHandler(w http.ResponseWriter, r *http.Request, title string) {
-	p, err := loadPage(title)
-	if err != nil {
-		p = &Page{Title: title}
-	}
-	renderTemplate(w, "edit", p)
-}
-
-func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
-	body := r.FormValue("body")
-	p := &Page{Title: title, Body: []byte(body)}
-	err := p.save()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	http.Redirect(w, r, "/view/"+title, http.StatusFound)
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	err := templates.ExecuteTemplate(w, tmpl+".html", p)
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	err := templates.ExecuteTemplate(w, tmpl+".html", nil)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
@@ -99,7 +39,5 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 func main() {
 	http.HandleFunc("/", homePageHandler)
 	http.HandleFunc("/admin/", adminPageHandler)
-	/*http.HandleFunc("/edit/", makeHandler(editHandler))
-	http.HandleFunc("/save/", makeHandler(saveHandler))*/
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
